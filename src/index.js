29603c898a3d469f48081bf647e3d07b54577046
Throsby Wells <throsbywells@gmail.com>
2022-03-24 10:20:32 -0400

    commit tree 97524218af86b4ffee2c9c5e293ab2dfc39286af

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID')
+        console.log(input.value)
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+      });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
